368cc4ce9677c82b544b35dea6ff554eae29de15
Thenemex <tnmx777@gmail.com>
2025-05-27 16:41:03 +0200

    Edited Logger method to make debug easier

--- src/main/java/tcreborn/api/Logger.java
+++ src/main/java/tcreborn/api/Logger.java
@@ -9,8 +9,11 @@ public class Logger {
 
     private static final org.apache.logging.log4j.Logger logger = LogManager.getLogger(modID);
 
-    public static void logInfo(Object message) {
-        logger.log(Level.INFO, modID.concat(" : ").concat(message.toString()));
+    public static void logInfo(Object ... messages) {
+        String message = "";
+        for (Object o : messages)
+            message = message.concat(o.toString());
+        logger.log(Level.INFO, modID.concat(" : ").concat(message));
     }
     public static void logError(Object message) {
         logger.log(Level.ERROR, modID.concat(" : ").concat(message.toString()));
