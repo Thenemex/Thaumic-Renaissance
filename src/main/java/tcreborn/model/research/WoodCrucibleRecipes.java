9022489a7f59a815669d25c0a31d8d94d33a04b3
Thenemex <tnmx777@gmail.com>
2025-06-10 19:03:45 +0200

    Added Crucible recipes for Planks and Sticks.

--- src/main/java/tcreborn/model/research/WoodCrucibleRecipes.java
+++ src/main/java/tcreborn/model/research/WoodCrucibleRecipes.java
@@ -1,14 +1,26 @@
 package tcreborn.model.research;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import tcreborn.api.recipes.crucible.CrucibleAdder;
+import tcreborn.api.thaumcraft.aspects.Aspects;
 import tcreborn.api.thaumcraft.research.AResearch;
 import tcreborn.model.config.ConfigTab;
 import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.crafting.CrucibleRecipe;
 import thaumcraft.api.research.ResearchPage;
 
+import java.util.Arrays;
+
 import static tcreborn.api.items.ItemFinder.findItemTC;
+import static tcreborn.model.ArrayCollector.getMagicalLogsToPlanks;
+import static tcreborn.model.config.ConfigOreDict.*;
 import static thaumcraft.api.aspects.Aspect.*;
 
 public class WoodCrucibleRecipes extends AResearch {
+
+    protected Aspects mundane, magical;
 
     public WoodCrucibleRecipes() {
         super(ConfigTab.lumberjack, "WOODCRUCIBLERECIPES", findItemTC("blockMetalDevice", 0));
@@ -16,14 +28,38 @@ public class WoodCrucibleRecipes extends AResearch {
 
     @Override
     public void init() {
+        this.mundane = new Aspects(new Aspect[]{TREE, CRAFT, ENTROPY}, 3, 1, 1);
+        this.magical = new Aspects(new Aspect[]{TREE, CRAFT, ENTROPY}, 4, 2, 2);
         this.setResearchAspects(new Aspect[]{TREE, MAGIC, EARTH}, 6, 3, 3);
         this.setNewResearch(4, -2, 1);
-        this.setPages(new ResearchPage(research.getPageTag(1)));
+        this.setPages(new ResearchPage(research.getPageTag(1)),
+                new ResearchPage(addRecipesMundanePlanks()),
+                new ResearchPage(addRecipesMundaneSticks()),
+                new ResearchPage(addRecipesMagicalPlanks()),
+                new ResearchPage(addRecipesMagicalSticks()));
         // Recipes already removed in WoodBasicRecipes
+    }
+
+    protected CrucibleRecipe[] addRecipesMundanePlanks() {
+         return CrucibleAdder.addMultipleRecipe(tag, mundane, getOres(mundanePlanksTag), 4, getOres(mundaneLogsTag));
+    }
+    protected CrucibleRecipe[] addRecipesMundaneSticks() {
+        return CrucibleAdder.addMultipleSingleRecipe(tag, mundane, new ItemStack(Items.stick, 4), getOres(mundanePlanksTag));
+    }
+    protected CrucibleRecipe[] addRecipesMagicalPlanks() {
+        CrucibleRecipe[] recipes = CrucibleAdder.addMultipleRecipe(tag, magical, getMagicalLogsToPlanks(), 6, getOres(magicalLogsTag));
+        return Arrays.copyOfRange(recipes, 0, 2);
+    }
+    protected CrucibleRecipe[] addRecipesMagicalSticks() {
+        CrucibleRecipe[] recipes = CrucibleAdder.addMultipleSingleRecipe(tag, magical, new ItemStack(Items.stick, 6), getOres(magicalPlanksTag));
+        return Arrays.copyOfRange(recipes, 0, 2);
     }
 
     @Override
     public void setResearchProperties() {
-        this.research.setParents("WOODARCANERECIPES");
+        this.research.setParents("WOODARCANERECIPES").setItemTriggers(
+                new ItemStack(Blocks.log, 1, 0), // Oak Log
+                new ItemStack(Blocks.planks, 1, 0), // Oak Plank
+                new ItemStack(Items.stick)); // Stick
     }
 }
