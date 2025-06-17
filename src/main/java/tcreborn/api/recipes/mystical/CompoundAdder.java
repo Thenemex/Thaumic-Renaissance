e90d0f1adaf0194ed3c5f499768cee2a87526796
Thenemex <tnmx777@gmail.com>
2025-06-17 12:47:05 +0200

    Added null and size contracts in CompoundAdder

--- src/main/java/tcreborn/api/recipes/mystical/CompoundAdder.java
+++ src/main/java/tcreborn/api/recipes/mystical/CompoundAdder.java
@@ -1,6 +1,8 @@
 package tcreborn.api.recipes.mystical;
 
 import tcreborn.api.thaumcraft.aspects.Aspects;
+import tcreborn.api.util.exceptions.CompoundRecipeSizeIsDifferentFromStructure;
+import tcreborn.api.util.exceptions.ParameterIsNullOrEmpty;
 import thaumcraft.common.config.ConfigResearch;
 
 import java.util.Arrays;
@@ -11,6 +13,8 @@ import java.util.List;
 public class CompoundAdder {
 
     public static List addCompoundRecipe(String tag, Aspects aspects, int width, int height, int length, Object ... structure) {
+        if (tag == null || structure == null || structure.length == 0) throw new ParameterIsNullOrEmpty();
+        if (width * height * length != structure.length) throw new CompoundRecipeSizeIsDifferentFromStructure(width * height * length, structure.length);
         return registerRecipe(tag, Arrays.asList(aspects, width, height, length, Arrays.asList(structure)));
     }
 
