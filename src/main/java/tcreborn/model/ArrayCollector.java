80837595cb710aba17e57c23af8f5a22f5be5bc4
Thenemex (from Laptop) <tnmx777@gmail.com>
2025-06-18 18:13:16 +0200

    Added methods in ArrayCollector, for adding magical block logs

--- src/main/java/tcreborn/model/ArrayCollector.java
+++ src/main/java/tcreborn/model/ArrayCollector.java
@@ -9,12 +9,14 @@ import tcreborn.api.util.exceptions.ParameterIsNullOrEmpty;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static tcreborn.api.items.ItemFinder.findBlockTC;
 import static tcreborn.api.items.ItemFinder.findItemTC;
 
+@SuppressWarnings("unused")
 public class ArrayCollector {
 
     private static final ArrayList<ItemStack> mundaneLogs, mundanePlanks, magicalLogs, magicalPlanks, magicalLogsToPlanks;
-    private static final ArrayList<BlockType> mundaneBlockLogs;
+    private static final ArrayList<BlockType> mundaneBlockLogs, magicalBlockLogs;
     static {
         mundanePlanks = new ArrayList<>();
         mundaneLogs = new ArrayList<>();
@@ -23,12 +25,14 @@ public class ArrayCollector {
         magicalLogsToPlanks = new ArrayList<>();
 
         mundaneBlockLogs = new ArrayList<>();
+        magicalBlockLogs = new ArrayList<>();
     }
 
     public static void init() {
         // Add all metadata of vanilla logs/planks
         ItemStack log;
         Block block;
+        BlockType blockType;
         for (int i = 0; i < 6; i++) {
             block = i < 4 ? Blocks.log : Blocks.log2;
             int meta = i < 4 ? i : i - 4;
@@ -37,8 +41,8 @@ public class ArrayCollector {
             addMundaneBlockLog(block, meta);
         }
         // Adding Magical Logs & Planks - Other will be added by integrated mods
-        addMagicalLog(findItemTC("blockMagicalLog", 0)); // Greatwood Log
-        addMagicalLog(findItemTC("blockMagicalLog", 1)); // Silverwood Log
+        addMagicalLogAndBlock(findBlockTC("blockMagicalLog", 0)); // Greatwood Log
+        addMagicalLogAndBlock(findBlockTC("blockMagicalLog", 1)); // Silverwood Log
         addSameMagicalResultPlank(findItemTC("blockWoodenDevice", 6)); // Greatwood Planks
         addSameMagicalResultPlank(findItemTC("blockWoodenDevice", 7)); // Silverwood Planks
     }
@@ -54,6 +58,12 @@ public class ArrayCollector {
     public static void addMagicalLog(ItemStack ... logs) {
         if (logs == null || logs.length == 0) throw new ParameterIsNullOrEmpty();
         magicalLogs.addAll(Arrays.asList(logs));
+    }
+    public static void addMagicalLogAndBlock(BlockType ... logs) {
+        for (BlockType V : logs) {
+            addMagicalBlockLog(V);
+            addMagicalLog(new ItemStack(V.block(), 1, V.meta()));
+        }
     }
     public static void addMagicalPlank(ItemStack ... planks) {
         if (planks == null || planks.length == 0) throw new ParameterIsNullOrEmpty();
@@ -90,8 +100,20 @@ public class ArrayCollector {
     public static void addMundaneBlockLog(Block block, int meta) {
         mundaneBlockLogs.add(new BlockType(block, meta));
     }
+    public static void addMundaneBlockLog(BlockType block) {
+        mundaneBlockLogs.add(block);
+    }
+    public static void addMagicalBlockLog(Block block, int meta) {
+        magicalBlockLogs.add(new BlockType(block, meta));
+    }
+    public static void addMagicalBlockLog(BlockType block) {
+        magicalBlockLogs.add(block);
+    }
 
     public static BlockType[] getMundaneBlockLogs() {
         return mundaneBlockLogs.toArray(new BlockType[0]);
+    }
+    public static BlockType[] getMagicalBlockLogs() {
+        return magicalBlockLogs.toArray(new BlockType[0]);
     }
 }
