8c2034775c4567535302a3d6ec5a1d37cd92c95e
Thenemex (from Laptop) <tnmx777@gmail.com>
2025-06-19 18:34:58 +0200

    The player now needs to be sneaking to trigger the Compound recipe

--- src/main/java/tcreborn/model/events/WoodCompoundRecipesHandler.java
+++ src/main/java/tcreborn/model/events/WoodCompoundRecipesHandler.java
@@ -22,20 +22,20 @@ public class WoodCompoundRecipesHandler extends WandEventHandler {
 
     @Override
     public boolean performTrigger(World world, ItemStack wand, EntityPlayer player, int x, int y, int z, int side, int event) {
-        return dropWoodPlanks(world, wand, x, y, z, event);
+        return dropWoodPlanks(world, wand, player, x, y, z, event);
     }
 
-    protected boolean dropWoodPlanks(World world, ItemStack heldItem,  int x, int y, int z, int event) {
+    protected boolean dropWoodPlanks(World world, ItemStack heldItem, EntityPlayer player, int x, int y, int z, int event) {
         if (world.isRemote) return false;
         ItemWandCasting wand = (ItemWandCasting) heldItem.getItem();
         if (wand.getFocus(heldItem) != null) return false; // Needs no focus equipped on the wand
+        if (!player.isSneaking()) return false; // Player needs to be sneaking
         // Code for editing world
         world.setBlockToAir(x, y, z);
         ItemStack item = getDrops(event);
         EntityItem drops = new EntityItem(world, (float) x + 0.5F, (float) y + 0.5F, (float) z + 0.5F, item);
         world.spawnEntityInWorld(drops);
         return true;
-        // ToDo : Make the research go auto-unlocked in Lumberjack Tab
         // ToDo : Review idea for 1.5 amount drop for expert mode
         // ToDo : Add condition for research done or not in performTrigger()
     }
