b2218b93f580ec67a6b081fc872adcba55005464
Thenemex <tnmx777@gmail.com>
2025-06-19 21:56:27 +0200

    Changed methods in ConfigResearch

--- src/main/java/tcreborn/model/config/ConfigResearch.java
+++ src/main/java/tcreborn/model/config/ConfigResearch.java
@@ -7,12 +7,17 @@ import static tcreborn.config.Config.expertWoodRecipesEnabled;
 public class ConfigResearch {
 
     public static void init() {
+        initLumberjackResearches();
+        // Add new AResearch here
+    }
+
+    protected static void initLumberjackResearches() {
         ConfigTab.initTab_LUMBERJACK();
         if (expertWoodRecipesEnabled) new WoodBasicRecipes();
         new WoodArcaneRecipes();
         new WoodCrucibleRecipes();
         new WoodCrucibleMagicalRecipes();
         new WoodCompoundRecipes();
+        new WoodCompoundMagicalRecipes();
     }
-        // Add new AResearch here
 }
