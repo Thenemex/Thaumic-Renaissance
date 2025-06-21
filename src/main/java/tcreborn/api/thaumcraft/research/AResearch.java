d4f1e61ebec4bbf3acafacc630af076c06fece40
Thenemex (from Laptop) <tnmx777@gmail.com>
2025-06-21 12:41:51 +0200

    Added getter for Research Aspects in AResearch

--- src/main/java/tcreborn/api/thaumcraft/research/AResearch.java
+++ src/main/java/tcreborn/api/thaumcraft/research/AResearch.java
@@ -6,6 +6,7 @@ import net.minecraft.item.ItemStack;
 import tcreborn.api.events.WandEventHandler;
 import tcreborn.api.thaumcraft.API;
 import tcreborn.api.thaumcraft.aspects.Aspects;
+import tcreborn.api.util.exceptions.FieldIsNull;
 import tcreborn.api.util.exceptions.ParameterIsNullOrEmpty;
 import tcreborn.config.Config;
 import thaumcraft.api.aspects.Aspect;
@@ -49,6 +50,10 @@ public abstract class AResearch {
     public abstract void setResearchProperties();
     public void removeRecipes() {}
 
+    public Aspects getAspects() throws FieldIsNull {
+        if (aspects == null) throw new FieldIsNull();
+        return aspects;
+    }
     public AResearch setResearchAspects(Aspect aspect, int amount) {
         this.aspects = new Aspects(aspect, amount);
         return this;
@@ -85,7 +90,8 @@ public abstract class AResearch {
         return research.getPages().length;
     }
 
-    public WandEventHandler getHandler() {
+    public WandEventHandler getHandler() throws FieldIsNull {
+        if (handler == null) throw new FieldIsNull();
         return handler;
     }
     public AResearch setHandler(WandEventHandler handler) {
