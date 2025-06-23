b12b66334d6b3abc2c2f69b1cc56913948c35be1
Thenemex (from Laptop) <tnmx777@gmail.com>
2025-06-23 13:14:08 +0200

    Added JDOC for BlockType. Implemented toString() method.

--- src/main/java/tcreborn/api/items/types/BlockType.java
+++ src/main/java/tcreborn/api/items/types/BlockType.java
@@ -2,20 +2,45 @@ package tcreborn.api.items.types;
 
 import net.minecraft.block.Block;
 
+/**
+ * Class used for storing the Block and it's metadata.
+ */
 public class BlockType {
 
-    private final Block block;
-    private final int meta;
+    protected final Block block;
+    protected final int meta;
 
+    /**
+     * Constructor for the class
+     * @param block The block
+     * @param meta The metadata
+     */
     public BlockType(Block block, int meta) {
         this.block = block;
         this.meta = Math.max(meta, 0);
     }
 
+    /**
+     * Getter for the block
+     * @return The block
+     */
     public Block block() {
         return block;
     }
+    /**
+     * Getter for the metadata
+     * @return The metadata
+     */
     public int meta() {
         return meta;
+    }
+
+    /**
+     * Convert the instance into text
+     * @return The instance turned into text
+     */
+    @Override
+    public String toString() {
+        return getClass().getSimpleName().concat("{block = " + block() + ", meta = " + meta + "}");
     }
 }
