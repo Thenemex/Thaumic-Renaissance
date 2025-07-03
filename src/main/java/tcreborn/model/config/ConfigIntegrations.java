bf73f531d6ffd95455830051834e30869484178e
Thenemex <tnmx777@gmail.com>
2025-07-03 16:19:23 +0200

    Added dependency to load after Thaumcraft 4 Tweaks

--- src/main/java/tcreborn/model/config/ConfigIntegrations.java
+++ src/main/java/tcreborn/model/config/ConfigIntegrations.java
@@ -3,13 +3,14 @@ package tcreborn.model.config;
 import cpw.mods.fml.common.Loader;
 import tcreborn.model.integrations.ForbiddenMagicCompat;
 import tcreborn.model.integrations.TaintedMagicCompat;
+import tcreborn.model.integrations.Thaumcraft4TweaksCompat;
 import tcreborn.model.integrations.ThaumicBasesCompat;
 
 import static tcreborn.config.Config.*;
 
 public class ConfigIntegrations {
 
-    private static final String forbiddenMagic = "ForbiddenMagic", taintedMagic = "TaintedMagic", thaumicBases = "thaumicbases";
+    private static final String forbiddenMagic = "ForbiddenMagic", taintedMagic = "TaintedMagic", thaumicBases = "thaumicbases", thaumcraft4Tweaks = "tc4tweak";
 
     public static void init() {
         if (modIsLoaded(forbiddenMagic, forbiddenMagicCompat))
@@ -18,6 +19,8 @@ public class ConfigIntegrations {
             new TaintedMagicCompat(taintedMagic);
         if (modIsLoaded(thaumicBases, thaumicBasesCompat))
             new ThaumicBasesCompat(thaumicBases);
+        if (modIsLoaded(thaumcraft4Tweaks, thaumcraft4TweaksCompat));
+            new Thaumcraft4TweaksCompat(thaumcraft4Tweaks);
     }
 
     public static boolean modIsLoaded(String mod, boolean config) {
