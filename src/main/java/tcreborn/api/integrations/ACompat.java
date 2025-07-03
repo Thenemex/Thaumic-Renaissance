a851b0148e7312f307e9585f61990e797881d342
Thenemex <tnmx777@gmail.com>
2025-07-03 16:37:16 +0200

    Added logging INFO for loading TCReborn integrations

--- src/main/java/tcreborn/api/integrations/ACompat.java
+++ src/main/java/tcreborn/api/integrations/ACompat.java
@@ -1,5 +1,7 @@
 package tcreborn.api.integrations;
 
+import tcreborn.api.util.Logger;
+
 public abstract class ACompat {
 
     protected final String mod;
@@ -7,6 +9,7 @@ public abstract class ACompat {
     public ACompat(String mod) {
         this.mod = mod;
         init();
+        Logger.logInfo("Successfully loaded integration for mod : ".concat(mod));
     }
 
     public void init() {
