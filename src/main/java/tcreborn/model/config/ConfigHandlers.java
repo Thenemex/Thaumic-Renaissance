264a90290793868ace5e342a0a94d382510a73ef
Thenemex <tnmx777@gmail.com>
2025-07-16 18:19:51 +0200

    Changed types of handlers in ConfigHandlers

--- src/main/java/tcreborn/model/config/ConfigHandlers.java
+++ src/main/java/tcreborn/model/config/ConfigHandlers.java
@@ -1,12 +1,12 @@
 package tcreborn.model.config;
 
-import tcreborn.api.events.WandEventHandler;
+import tcreborn.api.events.SingleBlockWithDropsHandler;
 import tcreborn.model.events.WoodCompoundMagicalRecipesHandler;
 import tcreborn.model.events.WoodCompoundRecipesHandler;
 
 public class ConfigHandlers {
 
-    public static WandEventHandler mundaneWoodCompoundHandler, magicalWoodCompoundHandler;
+    public static SingleBlockWithDropsHandler mundaneWoodCompoundHandler, magicalWoodCompoundHandler;
 
     public static void init() {
         mundaneWoodCompoundHandler = new WoodCompoundRecipesHandler();
