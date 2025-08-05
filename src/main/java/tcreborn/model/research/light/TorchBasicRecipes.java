dc4e5ee63a1d147a879b1fddd25ace4b9acc3912
Thenemex <tnmx777@gmail.com>
2025-08-05 22:08:40 +0200

    Added recipe for Glowstone torches, and showing in proper research

--- src/main/java/tcreborn/model/research/light/TorchBasicRecipes.java
+++ src/main/java/tcreborn/model/research/light/TorchBasicRecipes.java
@@ -1,10 +1,13 @@
 package tcreborn.model.research.light;
 
 import nemexlib.api.recipes.RecipeFinder;
+import nemexlib.api.recipes.workbench.WorkbenchAdder;
 import nemexlib.api.thaumcraft.research.AResearch;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
+import tcreborn.config.Config;
 import tcreborn.model.config.ConfigTab;
 import thaumcraft.api.research.ResearchPage;
 
@@ -17,10 +20,14 @@ public class TorchBasicRecipes extends AResearch {
     public void init() {
         this.setNewResearch(2, 0).setPages(new ResearchPage(research.getPageTag(1)),
                 new ResearchPage(getTorchRecipe()));
+        if (Config.glowstoneTorches) this.addPage(new ResearchPage(addGlowstoneTorchRecipe()));
     }
 
     protected IRecipe getTorchRecipe() {
         return RecipeFinder.findRecipeAmount(new ItemStack(Blocks.torch, 4));
+    }
+    protected IRecipe addGlowstoneTorchRecipe() {
+        return WorkbenchAdder.addRecipe(new ItemStack(Blocks.torch, Config.glowstoneTorchesOutput),false, "G", "S", 'G', Items.glowstone_dust, 'S', Items.stick);
     }
 
     @Override
