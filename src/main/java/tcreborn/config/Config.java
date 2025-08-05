f6d46b9c89f53bdb6bac5dfead374f8331c96e84
Thenemex <tnmx777@gmail.com>
2025-08-05 22:06:42 +0200

    Added config entry to turn on/off glowstone torches, and to tweak their output amount when crafted

--- src/main/java/tcreborn/config/Config.java
+++ src/main/java/tcreborn/config/Config.java
@@ -9,6 +9,8 @@ public class Config extends AConfig {
 
     public static boolean expertWoodRecipesEnabled;
     public static boolean forbiddenMagicCompat, taintedMagicCompat, thaumicBasesCompat, twilightForestCompat, thaumcraft4TweaksCompat;
+    public static boolean glowstoneTorches;
+    public static int glowstoneTorchesOutput;
 
     public Config(FMLPreInitializationEvent event, String version) {
         super(event, modName, modName, version);
@@ -16,14 +18,19 @@ public class Config extends AConfig {
 
     @Override
     protected void loadConfig() {
-        String integCat = "Integrations", expertCat = "Expert";
+        String integCat = "Integrations", expertCat = "Expert", additRecipes = "Additionnal-Recipes";
         config.addCustomCategoryComment(expertCat, "You can disable/enable expert configurations from the mod here.");
         expertWoodRecipesEnabled = newEntry(expertCat,"Planks/Sticks recipes", false);
+
         config.addCustomCategoryComment(integCat, "You can disable/enable mod integrations here.");
         forbiddenMagicCompat = newEntry(integCat, "Forbidden Magic");
         taintedMagicCompat = newEntry(integCat, "Tainted Magic");
         thaumicBasesCompat = newEntry(integCat, "Thaumic Bases");
         twilightForestCompat = newEntry(integCat, "Twilight Forest");
         thaumcraft4TweaksCompat = newEntry(integCat, "Thaumcraft 4 Tweaks");
+
+        config.addCustomCategoryComment(additRecipes, "You can disable/enable additionnal recipes here.");
+        glowstoneTorches = newEntry(additRecipes, "Glowstone Torches", "Allows for crafting torches from stick and glowstone");
+        glowstoneTorchesOutput = newEntry(additRecipes, "Glowstone Torches output", 5);
     }
 }
