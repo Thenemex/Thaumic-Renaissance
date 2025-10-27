ecf9b82aa9a9bf93843cfc2fb23b490662202eea
Thenemex <tnmx777@gmail.com>
2025-10-27 23:09:59 +0100

    Added new code in ConfigHandlers for rotated wood casting compatibility (Mundane only)

--- src/main/java/tcreborn/model/config/ConfigHandlers.java
+++ src/main/java/tcreborn/model/config/ConfigHandlers.java
@@ -1,6 +1,8 @@
 package tcreborn.model.config;
 
 import nemexlib.api.events.SingleBlockWithDropsHandler;
+import nemexlib.api.items.types.BlockType;
+import tcreborn.model.ArrayCollector;
 import tcreborn.model.events.WoodCompoundMagicalRecipesHandler;
 import tcreborn.model.events.WoodCompoundRecipesHandler;
 
@@ -11,5 +13,16 @@ public class ConfigHandlers {
     public static void init() {
         mundaneWoodCompoundHandler = new WoodCompoundRecipesHandler();
         magicalWoodCompoundHandler = new WoodCompoundMagicalRecipesHandler("WOODCOMPOUNDMAGICALRECIPES");
+        patchRotatedMundaneLogs();
+    }
+
+    protected static void patchRotatedMundaneLogs() {
+        // We consider any mundane wood log have his rotated selves at metadata +4 and +8
+        int i = 0;
+        for (BlockType V : ArrayCollector.getMundaneBlockLogs()) {
+            BlockType U = new BlockType(V);
+            mundaneWoodCompoundHandler.registerTriggerEvent(U.addMeta(4), i);
+            mundaneWoodCompoundHandler.registerTriggerEvent(U.addMeta(4), i++);
+        }
     }
 }
